1db26db2d8abf7a36cd85066a328b2e43c1b7bfa
Cartori21 <contactcartori@gmail.com>
2025-08-04 03:00:59 +0200

    ef

--- cartorimedia/.next/static/chunks/src_app_(public)_thumbnails_page_tsx_54f1ab13._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_a06da6fc._.js",
-  "static/chunks/node_modules_65ad0759._.js"
-],
-    source: "dynamic"
-});
